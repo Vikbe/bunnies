4ca1c2921f2aa867d846fe63f3d48dc8915d219b
Viktor Berglind <berglind96@gmail.com>
2017-12-27 02:16:40 +0100

    lots of stuff

--- Bunnies/Bunnies.h
+++ Bunnies/Bunnies.h
@@ -1,31 +1,37 @@
 #include <string> 
-#include <ctime>
+#include <ctime> 
+#include <vector>
 
-int randInt(int x, int y)   // Returns a random number from x-y 
+static int randInt(int x, int y)   // Returns a random number from x-y 
 {
-	srand((unsigned)std::time(0));
 	int number = rand() % y + x;
 	return number;
 }
 
-std::string randomName()
+static std::string randomName()
 {
 	std::string nameList[10] = { "Thumper", "Oreo", "Coco", "Peanut", "Pepper", "Fluffy", "Ginger", "Cookie", "Hazel", "Nibbles" };
 	return nameList[randInt(0, 9)];
 }
 
-bool radioActiveChance()
+static bool radioActiveChance()
 {
 	if (randInt(1, 100) == 1 || randInt(1, 100) == 2)
 		return true;
 	else
 		return false;
-}
+} 
+
+static void firstBunnies(std::vector<Bunny>& b);
 
 class Bunny
 {
 public:
-	Bunny();
+	Bunny(); 
+	Bunny(Bunny* m); 
+
+	void newTurn(std::vector<Bunny>& b); 
+	bool maleAlive(const std::vector<Bunny>& b);
 
 	enum Color
 	{
@@ -36,13 +42,15 @@ public:
 	};
 
 	enum Sex { Male, Female };
-
+	
 private:
 	std::string _name;
 	Color _color;
 	Sex _sex;
 	int _age;
-	bool _isRadioactive;
+	bool _isRadioactive; 
+	bool _alive;
+	Bunny* _mother;
 };
 
 
--- Bunnies/Source.cpp
+++ Bunnies/Source.cpp
@@ -1,7 +1,26 @@
+#include "Bunnies.h"
+#include <iostream>
 
+void firstBunnies(std::vector<Bunny>& b)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		b.push_back(Bunny());
+	}
+}
 
 int main()
 {
+	srand((unsigned)std::time(0));
+	
+	std::vector<Bunny> bunnies;
+	firstBunnies(bunnies); 
+
+	for (auto i : bunnies)
+	{
+		std::cout << i._name << std::endl;
+	}
+
 	return 0;
 }
 
